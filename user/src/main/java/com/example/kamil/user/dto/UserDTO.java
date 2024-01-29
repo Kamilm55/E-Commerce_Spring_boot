269f8939ce27dc64b99e63e402c6bb7e34f34edd
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2024-01-30 01:05:11 +0400

    Create UserDTO.java

---
+++ user/src/main/java/com/example/kamil/user/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package com.example.kamil.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+
+	
+	private String name;
+	
+	private String surname;
+	
+	private String username;
+	
+	private String password;
+	
+	private String email;
+
+}
