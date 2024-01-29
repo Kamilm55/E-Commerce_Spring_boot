b4f210828cc723fc839000fd96736c585fe520bb
Kamilm55 <kamilmmmdov2905@gmail.com>
2024-01-30 00:45:45 +0400

    controller add

--- user/src/main/java/com/example/kamil/user/controller/UserController.java
+++ user/src/main/java/com/example/kamil/user/controller/UserController.java
@@ -1,4 +1,21 @@
 package com.example.kamil.user.controller;
 
+import com.example.kamil.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/v1/auth")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<List<User>> getAll(){
+        return ResponseEntity.ok(userService.getAll());
+    }
 }
