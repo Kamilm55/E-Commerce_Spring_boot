b198e0e96122cfe7423c9e1b771a19888fd07ba4
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2024-01-30 01:14:30 +0400

    usercontroller

--- user/src/main/java/com/example/kamil/user/controller/UserController.java
+++ user/src/main/java/com/example/kamil/user/controller/UserController.java
@@ -1,4 +1,26 @@
 package com.example.kamil.user.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.kamil.user.dto.UserDTO;
+import com.example.kamil.user.entity.UserEntity;
+
+@RestController
+@RequestMapping(path = "users")
 public class UserController {
+	
+	@PostMapping
+	public void add(@RequestBody UserDTO u ) {
+		UserEntity entity = new UserEntity();
+		entity.setId(null);
+		entity.setName(u.getName());
+		entity.setSurname(u.getSurname());
+		entity.setUsername(u.getUsername());
+		entity.setPassword(u.getPassword());
+		entity.setEmail(u.getEmail());
+		
+	}
 }
--- user/src/main/java/com/example/kamil/user/entity/UserEntity.java
+++ user/src/main/java/com/example/kamil/user/entity/UserEntity.java
@@ -1,6 +1,8 @@
 package com.example.kamil.user.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -14,9 +16,14 @@ import lombok.ToString;
 @ToString
 public class UserEntity  {
 
-
-	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+
+	private String surname;
+	
 	private String username;
 	
 	private String password;
