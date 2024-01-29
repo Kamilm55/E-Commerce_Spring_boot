a9873bd2a7c4db0d6728ff10180662a6accc23d1
Kamilm55 <kamilmmmdov2905@gmail.com>
2024-01-29 22:29:43 +0400

    multi module spring boot app initialized

---
+++ main/src/main/java/com/example/kamil/main/MainApplication.java
@@ -0,0 +1,15 @@
+package com.example.kamil.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.example.kamil","com.example.kamil.user"})
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
